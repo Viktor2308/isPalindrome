1f8e9e62f4467e0481310afb8578c3698b07404a
Viktor <viktor_23_08@mail.ru>
2022-12-22 16:14:29 +0100

    tiket 2 task 1

--- src/com/skypro/isPalinrome/Main.java
+++ src/com/skypro/isPalinrome/Main.java
@@ -2,6 +2,36 @@ package com.skypro.isPalinrome;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        //- Задача
+        //Напишите программу, которая проверяет, является ли строка палиндромом.
+        //(Слово или фраза, которые одинаково читаются слева направо и справа налево.)
+        String str1 = "";
+        String str2 = "1";
+        String str3 = "шалаш";
+        String str4 = "45565арв6434";
+
+        System.out.println("isPalindrome(str1) = " + isPalindrome(str1));
+        System.out.println("isPalindrome(str2) = " + isPalindrome(str2));
+        System.out.println("isPalindrome(str3) = " + isPalindrome(str3));
+        System.out.println("isPalindrome(str4) = " + isPalindrome(str4));
+    }
+
+    public static boolean isPalindrome(String str) {
+        boolean b = true;
+        if (str.length() == 0 || str.length() == 1) {
+            b = true;
+
+        } else {
+            int start = 0;
+            int end = str.length() - 1;
+            while (start <= end) {
+                if (str.charAt(start) != str.charAt(end)) {
+                    b = false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return b;
     }
 }
